dd6184b5a708ed70bd587150ed6b52ee655fd017
Michał 'acid' Bagrowski <michal@bagrowski.com>
2017-09-14 22:50:05 +0200

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+setup(
+    name='ebay_lib',
+    author="Michał Bagrowski",
+    author_email="michal@bagrowski.com",
+    version='0.0.1',
+    include_package_data=True,
+    description="trollol",
+    packages=find_packages(),
+    install_requires=reqs,
+    dependency_links=[]
+)
